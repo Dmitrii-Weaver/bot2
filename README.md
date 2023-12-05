23c97f67691c338f02cbfd9862ae74215694726c
Weaver <cordies1106@gmail.com>
2023-12-05 04:11:04 +0200

    YT fetch WIP
    YT fetch WIP

--- README.md
+++ README.md
@@ -5,5 +5,7 @@ Older versions don't work anymore since discord API was changed
 
 CURRENT FUNCTIONS :
     - Reads and replies to messages on servers and in DMs
+    - can fetch the frames/ongoing lives of Holomems with short text commands
+    - currently working on fetching tweets
 
 requires a token to be used. it's not here, duh, I'm not dumb
--- responses.py
+++ responses.py
@@ -2,10 +2,49 @@ import random
 
 def get_response(message: str) -> str :
     p_message = message.lower()
-    if p_message == "hello" :
+    if p_message == "!hello" :
         return "Hey there!"
-    if p_message == "roll":
+    if p_message == "!roll":
         return str(random.randint(1,6))
     if p_message == "!help":
         return " `YOUR MESSAGE IN HERE` "
-    return 'idk lmao, type "!help" or smth '
+    
+
+    
+    if p_message[:5] == "!tweet":
+        return "it no work yet, lmao"
+
+    #live stream/freechat get
+    if p_message[:6] == "!frame":
+        if p_message.split(" ")[1] == "ame" or p_message.split(" ")[1] == "amelia" :
+            return "https://www.youtube.com/@WatsonAmelia/live"
+        if p_message.split(" ")[1] == "gura" :
+            return "https://www.youtube.com/@GawrGura/live"
+        if p_message.split(" ")[1] == "calli" or p_message.split(" ")[1] == "mori" :
+            return "https://www.youtube.com/@MoriCalliope/live"
+        if p_message.split(" ")[1] == "kiara" or p_message.split(" ")[1] == "wawa" or p_message.split(" ")[1] == "kiawa":
+            return "https://www.youtube.com/@TakanashiKiara/live"
+        if p_message.split(" ")[1] == "ina" :
+            return "https://www.youtube.com/@NinomaeInanis/live"
+        if p_message.split(" ")[1] == "irys" :
+            return "https://www.youtube.com/@IRyS/live"
+        if p_message.split(" ")[1] == "bae" or p_message.split(" ")[1] == "baelz" : 
+            return "https://www.youtube.com/@HakosBaelz/live"
+        if p_message.split(" ")[1] == "kronii" :
+            return "https://www.youtube.com/@OuroKronii/live"
+        if p_message.split(" ")[1] == "mumei" or p_message.split(" ")[1] == "moom" or p_message.split(" ")[1] == "moomers" :
+            return "https://www.youtube.com/@NanashiMumei/live"
+        if p_message.split(" ")[1] == "fauna" :
+            return "https://www.youtube.com/@CeresFauna/live"
+        if p_message.split(" ")[1] == "FWMK" or p_message.split(" ")[1] == "fuwamoco" or p_message.split(" ")[1] == "fuwawa" or p_message.split(" ")[1] == "mococo":
+            return "https://www.youtube.com/@FUWAMOCOch/live"
+        if p_message.split(" ")[1] == "shiori" or p_message.split(" ")[1] == "shiorin" :
+            return "https://www.youtube.com/@ShioriNovella/live"
+        if p_message.split(" ")[1] == "bijou" or p_message.split(" ")[1] == "biboo" :
+            return "https://www.youtube.com/@KosekiBijou/live"
+        if p_message.split(" ")[1] == "nerissa" or p_message.split(" ")[1] == "risa" :
+            return "https://www.youtube.com/@NerissaRavencroft/live"
+
+
+    if p_message == "!freechat":
+        return "https://www.youtube.com/watch?v=xcdlTH2FZEo&list=PLdV-bBPbIWm44iC9TfwtA8TqS-TC5nT24&index=1"
